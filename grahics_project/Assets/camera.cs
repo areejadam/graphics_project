4179882d78e67aced1d0277ad6e1b7e09612f0d7
areej <areejadam0@gmail.com>
2018-04-28 00:28:21 +0200

    env_and_assia

---
+++ grahics_project/Assets/camera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour {
+
+    public Transform PlayerTransform;
+
+    private Vector3 _cameraOffset;
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+    public bool LookAtPlayer = false;
+	// Use this for initialization
+
+	void Start () {
+        _cameraOffset = transform.position - PlayerTransform.position;
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newPos = PlayerTransform.position + _cameraOffset;
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+        if (LookAtPlayer)
+            transform.LookAt(PlayerTransform);
+		
+	}
+}
